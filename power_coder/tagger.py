6fb0d39f812ddd8a7b3e83a2c5ee464b65838cf9
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-07-08 16:59:46 +0530

    started building the preprocessor

--- power_coder/tagger.py
+++ power_coder/tagger.py
@@ -8,5 +8,3 @@ class DataStructureTagger:
     def __init__(self, model="rfc"):
 
         self.model = pickle.load(file="https://github.com/SkAndMl/question_tagging/raw/main/models/gb_max_depth_26_n_estimators_1019_lr_0.82.sav")
-    
-    # def predict(self, ):
